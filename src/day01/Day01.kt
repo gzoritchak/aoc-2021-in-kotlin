b8ec24d45cefd27e52f4e926e259f3061b74ce4a
gzoritchak <g.zoritchak@gmail.com>
2021-12-04 09:42:25 +0100

    package per day structure

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,3 +1,7 @@
+package day01
+
+import readInput
+
 fun main() {
     fun part1(input: List<Int>): Int = input
         .windowed(2)
@@ -9,12 +13,12 @@ fun main() {
         .count { (a,b) -> a < b }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test").map { it.toInt() }
+    val testInput = readInput("day01/Day01_test").map { it.toInt() }
 
     check(part1(testInput) == 7)
     check(part2(testInput) == 5)
 
-    val input = readInput("Day01").map { it.toInt() }
+    val input = readInput("day01/Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -1,3 +1,7 @@
+package day02
+
+import readInput
+
 fun main() {
     fun part1(input: List<Command>): Int = input
         .fold(SubmarineState()) { state, command -> state.applyCommandPart1(command)}
@@ -8,12 +12,12 @@ fun main() {
         .let { it.position.x * it.position.y }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day02_test").map { it.toCommand() }
+    val testInput = readInput("day02/Day02_test").map { it.toCommand() }
 
     check(part1(testInput) == 150)
     check(part2(testInput) == 900)
 
-    val input = readInput("Day02").map { it.toCommand() }
+    val input = readInput("day02/Day02").map { it.toCommand() }
     println(part1(input))
     println(part2(input))
 }
@@ -21,16 +25,16 @@ fun main() {
 
 fun SubmarineState.applyCommandPart1(command: Command): SubmarineState =
     when(command) {
-        is Command.Forward  -> copy( position = position.copy(x = position.x + command.value))
-        is Command.Down     -> copy( position = position.copy(y = position.y + command.value))
-        is Command.Up       -> copy( position = position.copy(y = position.y - command.value))
+        is Command.Forward -> copy( position = position.copy(x = position.x + command.value))
+        is Command.Down -> copy( position = position.copy(y = position.y + command.value))
+        is Command.Up -> copy( position = position.copy(y = position.y - command.value))
     }
 
 fun SubmarineState.applyCommandPart2(command: Command): SubmarineState =
     when(command) {
-        is Command.Down     -> copy( aim = aim + command.value)
-        is Command.Up       -> copy( aim = aim - command.value)
-        is Command.Forward  -> copy( position = position.copy(
+        is Command.Down -> copy( aim = aim + command.value)
+        is Command.Up -> copy( aim = aim - command.value)
+        is Command.Forward -> copy( position = position.copy(
                 x = position.x + command.value,
                 y = position.y + aim * command.value
         )
@@ -60,7 +64,7 @@ sealed class Command(val value: Int){
     class Up(x:Int)         : Command(x)
 }
 
-private fun String.toCommand():Command {
+private fun String.toCommand(): Command {
     val (command, value) = this.split(" ")
     return when(command) {
         "forward"   -> Command.Forward(value.toInt())
