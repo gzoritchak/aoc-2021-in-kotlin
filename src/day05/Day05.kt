3402a4cceac5ca33e996401b4fa3c09683994cee
gzoritchak <g.zoritchak@gmail.com>
2021-12-05 23:31:18 +0100

    day05: refactor

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -5,58 +5,36 @@ import java.lang.Integer.max
 import java.lang.Integer.min
 import kotlin.math.absoluteValue
 
+val winds: List<Wind> = readInput("day05/Day05").map { line ->
+    val (x0,y0,x1,y1) = line.replace(" -> ", ",").split(",").map { it.toInt() }
+    Wind(Point(x0,y0), Point(x1,y1))
+}
+
 fun main() {
-    println(count(readInput("day05/Day05")
-        .toWinds()
-        .filter { it.isVertical() || it.isHorizontal() }
-    ))
-    println(count(readInput("day05/Day05")
-        .toWinds()
-    ))
+    println(count(winds.filter { it.isVertical() || it.isHorizontal() }))
+    println(count(winds))
 }
 
-fun count(winds: List<Wind>): Int {
-    val count = mutableMapOf<Point, Int>()
-    winds
-        .flatMap { it.toPoints() }
-        .forEach {
-            val previous = count.getOrDefault(it, 0)
-            count[it] = previous + 1
-        }
-    return count.values.count { it>1 }
-}
-
-data class Point(val x: Int, val y: Int)
+fun count(winds: List<Wind>): Int = winds
+    .flatMap { it.toPoints() }
+    .fold(mutableMapOf<Point, Int>()) { acc, point -> acc[point] = acc.getOrDefault(point, 0) + 1; acc }
+    .values.count { it > 1 }
 
 data class Wind(val from: Point, val to: Point) {
     fun isVertical()    = from.x == to.x
     fun isHorizontal()  = from.y == to.y
+    fun size()          = if (isHorizontal()) (to.x - from.x).absoluteValue else (to.y - from.y).absoluteValue
 
-    fun toPoints(): List<Point> = when {
-        isHorizontal()  -> { (min(from.x, to.x)..max(from.x, to.x)).map { Point(it, from.y) } }
-        isVertical()    -> { (min(from.y, to.y)..max(from.y, to.y)).map { Point(from.x, it) } }
-        else            -> {
-            val dx = direction(from.x, to.x)
-            val dy = direction(from.y, to.y)
-            (0..(from.x - to.x).absoluteValue).map {
-                Point(from.x + it*dx, from.y + it*dy)
-            }
-        }
+    fun toPoints(): List<Point> {
+        val dx = direction(from.x, to.x)
+        val dy = direction(from.y, to.y)
+        return (0..size()).map { Point(from.x + it * dx, from.y + it * dy) }
     }
 
-    private fun direction(from: Int, to: Int): Int =
-        when {
+    private fun direction(from: Int, to: Int) = when {
             from > to -> -1
             from < to -> 1
             else -> 0
         }
-
 }
-
-const val delimiter = "->"
-
-private fun  List<String>.toWinds(): List<Wind> = map {
-    val (x0, y0) = it.substringBefore(delimiter).trim().split(",").map { it.toInt() }
-    val (x1, y1) = it.substringAfter(delimiter).trim().split(",").map { it.toInt() }
-    Wind(Point(x0,y0), Point(x1,y1))
-}
+data class Point(val x: Int, val y: Int)
