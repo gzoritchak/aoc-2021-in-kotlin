f4ecfe538e02d406a403ebb4bbf5f50c98e8ae85
gzoritchak <g.zoritchak@gmail.com>
2021-12-07 08:26:41 +0100

    day07: not optimized solution

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -6,9 +6,13 @@ import kotlin.math.absoluteValue
 val input = readInput("day07/Day07").first().split(",").map { it.toInt() }.sorted()
 
 fun main() {
-    println("Day 07:: ${input.sum() / input.size}  $input")
-    val min = (input.first()..input.last()).minOf { i->
-        input.sumOf { (i - it).absoluteValue }
-    }
-    println(min)
+    println( input.minDistanceBy (::cost1))
+    println( input.minDistanceBy (::cost2))
 }
+
+fun List<Int>.minDistanceBy(cost: (Int)->Int):Int =
+    (first()..last())
+        .minOf { i-> input.sumOf { cost(i - it) }}
+
+fun cost1(distance: Int):Int = distance.absoluteValue
+fun cost2(distance: Int):Int = distance.absoluteValue.let { (it + 1) * it/2 }
