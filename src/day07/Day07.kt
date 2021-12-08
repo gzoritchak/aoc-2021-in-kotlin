f7615a620da599b43ba861173ff5e6d9d494540a
gzoritchak <g.zoritchak@gmail.com>
2021-12-08 23:29:53 +0100

    day07: refactoring

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -3,16 +3,15 @@ package day07
 import readInput
 import kotlin.math.absoluteValue
 
-val input = readInput("day07/Day07").first().split(",").map { it.toInt() }.sorted()
+val sortedInput = readInput("day07/Day07").first().split(",").map { it.toInt() }.sorted()
 
 fun main() {
-    println( input.minDistanceBy (::cost1))
-    println( input.minDistanceBy (::cost2))
+    println( sortedInput.minimizeCost { it })
+    println( sortedInput.minimizeCost { it.arithmeticProgression()})
 }
 
-fun List<Int>.minDistanceBy(cost: (Int)->Int):Int =
+fun List<Int>.minimizeCost(costFunction: (Int)->Int):Int =
     (first()..last())
-        .minOf { i-> input.sumOf { cost(i - it) }}
+    .minOf { i-> sortedInput.sumOf { costFunction((i - it).absoluteValue) }}
 
-fun cost1(distance: Int):Int = distance.absoluteValue
-fun cost2(distance: Int):Int = distance.absoluteValue.let { (it + 1) * it/2 }
+fun Int.arithmeticProgression():Int =  (this + 1) * this /2
